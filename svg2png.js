6309ec75c450809a77cf7284d60bf1689d4e90d9
Nokorot <torhaakon2012@hotmail.com>
2019-01-11 23:11:26 +0000

    Latex compelation

--- svg2png.js
+++
@@ -1,9 +0,0 @@
-const { convertFile}  = require('convert-svg-to-png');
-
-(async() => {
-  const inputFilePath = 'res.svg';
-  const outputFilePath = await convertFile(inputFilePath, options={width:1000, scale=0.5});
-
-  console.log(outputFilePath);
-  //=> "/path/to/my-image.png"
-})();
